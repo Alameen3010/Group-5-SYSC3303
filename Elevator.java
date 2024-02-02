f4affa42e0f6f75642ed412f8c08bb7a61df1498
Al-ameen Alliu <alameenalliu@cmail.carleton.ca>
2024-02-01 22:47:07 -0500

    Update Elevator.java
    New changes made to the elevator class

--- Elevator.java
+++ Elevator.java
@@ -1,55 +1,107 @@
 
     /**
+     * @author Al-ameen Alliu, 101159780
      *The thread for the elevator class where data is collected from the scheduler,
      *processed and sent back o the scheduler
      */
-
     public class Elevator implements Runnable {
-        private Box boxFromScheduler; //Box used to receive message from the Schedule
-        private Box boxToScheduler; // Box used to communicate with the Scheduler
-
+        private Box boxFromScheduler; // Box used to receive messages from the Scheduler
+        private Box boxToScheduler; // Box used to send messages to the Scheduler
+        private int currentFloor; // The current floor where the elevator is
 
         public Elevator(Box boxFromScheduler, Box boxToScheduler) {
-            this.boxFromSchduler = boxFromScheduler;
+            this.boxFromScheduler = boxFromScheduler;
             this.boxToScheduler = boxToScheduler;
-            this.currentFloor = 1;
-
+            this.currentFloor = 1; // Starting on the first floor
         }
 
         public void run() {
             while (!Thread.currentThread().isInterrupted()) {
-                // Check for new messages from the Scheduler
-                EventData eventData = boxFromScheduler.getEventData();
+                // Assume Box class has synchronized get/set methods
+                Integer floorRequest = boxFromScheduler.getFloorNumber();
 
-                if (eventData != null) {
-                    // Process the message and take action
-                    processCommand(eventData);
+                //Boolean directionRequest = boxFromScheduler.getDirection();
+                //String timeRequest = boxFromScheduler.getTime();
 
-                    // After processing, send a response back to the Scheduler
-                    ProcessedData processedData = new ProcessedData(/* data */);
-                    boxToScheduler.setProcessedData(processedData);
+                if (floorRequest != null) {
+                    processFloorRequest(floorRequest);
+                    boxFromScheduler.setFloorNumber(null); // Clear the request after processing
                 }
 
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(1000); //
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
             }
         }
 
-        private void processCommand(EventData eventData) {
-            // For example, if the command is to move to a floor
-            moveToFloor(eventData.getFloorNumber());
+        private void processFloorRequest(Integer floorRequest) {
+            moveToFloor(floorRequest);
+            // After moving, send confirmation back to Scheduler
+            boxToScheduler.setCarButtonNumber(currentFloor);
         }
 
         private void moveToFloor(int floor) {
-            // Logic to move the elevator to the specified floor
-            this.currentFloor = floor; // Update the current floor
 
-
+            this.currentFloor = floor; // Set the current floor to the requested floor
         }
 
-        // Additional methods as needed...
+        // Get and Set methods for currentFloor for possible external access
+        public synchronized int getCurrentFloor() {
+            return currentFloor;
+        }
 
+        public synchronized void setCurrentFloor(int currentFloor) {
+            this.currentFloor = currentFloor;
+        }
     }
+
+
+    /**
+     *   class Box {
+     *         private Integer floorNumber;
+     *         private Boolean direction;
+     *         private Integer carButtonNumber;
+     *         private String time; // Time in the format "hh:mm:ss.mmm"
+     *
+     *
+     *         // Synchronized getter and setter for floorNumber
+     *         public synchronized Integer getFloorNumber() {
+     *             return floorNumber;
+     *         }
+     *
+     *         public synchronized void setFloorNumber(Integer floorNumber) {
+     *             this.floorNumber = floorNumber;
+     *         }
+     *
+     *             // Synchronized getter and setter for direction
+     *             public synchronized Boolean getDirection() {
+     *                 return direction;
+     *             }
+     *
+     *             public synchronized void setDirection(Boolean direction) {
+     *                 this.direction = direction;
+     *             }
+     *
+     *             // Synchronized getter and setter for carButtonNumber
+     *             public synchronized Integer getCarButtonNumber() {
+     *                 return carButtonNumber;
+     *             }
+     *
+     *             public synchronized void setCarButtonNumber(Integer carButtonNumber) {
+     *                 this.carButtonNumber = carButtonNumber;
+     *             }
+     *         // Synchronized getter for time
+     *         public synchronized String getTime() {
+     *             return time;
+     *         }
+     *
+     *         // Synchronized setter for time
+     *         public synchronized void setTime(String time) {
+     *             this.time = time;
+     *
+     *         }
+     *
+     *     }
+     */
