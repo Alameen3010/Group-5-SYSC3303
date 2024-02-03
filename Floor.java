a3a4ab31378fb63417c455df31d08c62ae857928
IlyesOutaleb <ilyesoutaleb@cmail.carleton.ca>
2024-02-03 13:20:19 -0500

    Update Floor.java
    Added full message format

--- Floor.java
+++ Floor.java
@@ -11,21 +11,25 @@ public class Floor implements Runnable {
     public void run() {
         for (int i = 0; i < 2; i++)
         {
-            getFromCSV("Message " + i);
+            getFromCSV("Message ", i, false, i + 2); //Will extract from CSV and put it into a new attribute call it message;
+
             getFromSchedulerResponse();
 
         }
         //getFromCSV("Message 1"); // get from csv and puts it to the box
         //putFromFloorToScheduler();
         //getFromSchedulerResponse();
-
+        System.exit(0);
 
 
     }
 
-    public void getFromCSV(String contnent)
-    {   //System.out.println("Sendind date to Scheduler");
-        sharedBox.getFromRecipient(contnent);
+    public void getFromCSV(String time, int floorNumber, boolean directionUp, int carButtonNumber)
+    {
+        Box temp = new Box(time, floorNumber, directionUp, carButtonNumber);
+        sharedBox.getFromSource(temp);
+        //System.out.println("Sendind date to Scheduler");
+        //sharedBox.getFromRecipient(contnent);
         //System.out.println("Sendind date to Scheduler");
 
     }
@@ -39,7 +43,9 @@ public class Floor implements Runnable {
     */
     public void getFromSchedulerResponse()
     {   //System.out.println("received response from Scheduler");
-        System.out.println(sharedBox.sendToRecipient());
+        Box temp = sharedBox.sendToSource();
+        temp.printContents();
+        //sharedBox.printContents();
         //System.out.println("received response from Scheduler");
     }
 }
