f940c936dafd09d7dcfcd630faf0af793d7093d2
IlyesOutaleb <ilyesoutaleb@cmail.carleton.ca>
2024-02-03 13:20:47 -0500

    Update Scheduler.java
    Added full message format

--- Scheduler.java
+++ Scheduler.java
@@ -1,17 +1,21 @@
 public class Scheduler implements Runnable{
 
     private Box sharedBox;
+    private Box sharedBox2;
+    private Box buffer;
 
-    private String buffer;
-
-    public Scheduler(Box box)
+    public Scheduler(Box box, Box box2)
     {
         this.sharedBox = box;
+        this.sharedBox2 = box2;
     }
 
     public void run() {
-        for (int i = 0; i < 2; i++) {
+        while(true){
+
             getFromFloor();
+            sendToElevator();
+            getFromElevator();
             sendToFloor();
         }
     }
@@ -22,9 +26,19 @@ public class Scheduler implements Runnable{
         //System.out.println("received message from Floor");
     }
 
+    private void sendToElevator()
+    {
+        sharedBox2.getFromSource(this.buffer);
+    }
+
+    private void getFromElevator()
+    {
+        this.buffer = sharedBox2.sendToSource();
+    }
+
     private void sendToFloor()
     {   //System.out.println("Sending message to Floor");
-        sharedBox.getFromDestination(buffer);
+        sharedBox.getFromDestination(this.buffer);
         //System.out.println("Sending message to Floor");
 
     }
