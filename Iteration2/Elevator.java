3df30554ecf0d90a205d8b7c2edfb96f49e1b6b6
IlyesOutaleb <ilyesoutaleb@cmail.carleton.ca>
2024-03-02 13:49:36 -0500

    Update Elevator.java
    Feat: Integrated Iteration 1 & Iteration 2 Code

--- Iteration2/Elevator.java
+++ Iteration2/Elevator.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-
 /**
- * The Elevator class which will implement all the actions required to move passengers from floor to floor.
- * The Elevator state machine starts with a scheduler request and finishes when all elevator Requests (done by passengers
- * inside the elevator) are processed. Then it stays IDLE and waits for another scheduler request.
- * @author Alameen Alliu (101159780)
- * @version 2024-02-17
+ * Elevator class handles the operations of an elevator as it receives commands from a scheduler.
+ * It runs within its own thread, processing incoming requests and sending responses.
+ *
+ * @author Al-ameen Alliu
+ * @version February 03, 2024,
+ * Edited: Ilyes Outaleb (101185290)
+ * @version March 02, 2024,
+ * Integrated it with Iteration 1
  */
+public class Elevator implements Runnable {
 
-public class Elevator {
-    /* Variable representing all the possible states of the Elevator. */
     private enum State {
         STOP,
         DOOR_OPENING,
@@ -22,32 +22,75 @@ public class Elevator {
     }
 
     private int floorRequested; /* Assuming the floor requested is retrieved from scheduler */
+
+    private int destinationFloor; /* The floor to which the passenger has asked to go */
     private int currentFloor; /* Variable representing on which the elevator is currently on. */
-    private State state; // Initial state is stop
-    private boolean elevatorButtonPressed; /*Variable representing if the button has been pressed inside the elevator*/
+
+    private int stopFloor; /* Buffer varaible to represent the destination floor of any given path of the state machine */
+    private State state; /* Initial state is stop */
+
+    private boolean elevatorHasPassenger; /* Boolean to represent whether the elevator is moving with or without passenger */
+    private Box sharedBox; // The shared Box for communication
+    private Box buffer; // Temporary buffer to store messages
 
     /**
-     * The constructor for the elevator class which sets the entry condition to STGP.
+     * Constructor for Elevator class.
+     *
+     * @param box The shared Box instance for communication between Scheduler and Elevator.
      */
-    public Elevator()
-    {
+    public Elevator(Box box) {
+        this.sharedBox = box;
         this.state = State.STOP;
+        this.currentFloor = 0;
+        this.elevatorHasPassenger = false;
     }
 
     /**
-     * Main class responsible for simulating the state machine for the elevator subsystem. This will transition from
-     * the different states of the machine under the right conditions/events.
+     * The main run method that is executed when the elevator thread starts.
+     * It continuously processes requests from the scheduler and sends responses.
      */
-    public void processSchedulerRequest() {
-        Scanner myObj = new Scanner(System.in);
+    public void run() {
+        while(true) {
+            receiveFromScheduler();
 
-        /* Will enter different case blocks according to the state attribute */
+            this.floorRequested = this.buffer.getFloorNumber(); /* Retrieves the data from the box to be used */
+            this.destinationFloor = this.buffer.getDestinationFloor();
+
+            /* Displays on which floor the elevator is starting the iteration */
+            System.out.println("The Elevator is starting at floor: " + this.currentFloor);
+
+            /* Continues while the state machine has not finished marked by the arrival of the passenger at destination */
+            while(!processSchedulerRequest())
+            {
+                /* If the elevator is where the passenger is pressing the floor button */
+                if (this.currentFloor == this.floorRequested)
+                {
+                    /* Then elevator must move to where the passenger wants to debark */
+                    this.stopFloor = this.destinationFloor;
+
+                }
+                else /* if that is not the case */
+                {   /* then it must move to where the passenger is located first */
+                    this.stopFloor = this.floorRequested;
+                }
+            };// only for one elevator change it to multiple by looking at iteration 2 original.
+
+            sendToSchedulerResponse();
+        }
+    }
+
+
+    public boolean processSchedulerRequest() {
+
         switch (this.state) {
             /* Will start at Stop and will transisient depending if its on the right floor or not */
             case State.STOP:
+
                 System.out.println("State: Stop");
                 System.out.println("Current Floor:" + this.currentFloor);
-                if (currentFloor == floorRequested)
+
+                /* If elevator is in requested or destination floor it must open to let passenger in or out */
+                if (currentFloor == floorRequested || currentFloor == destinationFloor)
                 {
                     this.state = State.DOOR_OPENING; /* If its on right floor just open door */
                 }
@@ -66,11 +109,18 @@ public class Elevator {
             /* This state is where the elevator has its door opens. Then checks if passenger entered */
             case State.DOOR_OPEN:
                 System.out.println("State: Door open");
-                this.elevatorButtonPressed = processResponse(); /* Simulate passenger entering and pressing elevator button */
-                if (elevatorButtonPressed) /* For the purpose of the simulation the timer condition is not implemented */
+
+                if (this.elevatorHasPassenger)             /* If the elevator has a passenger it must be debarking it */
                 {
-                    System.out.println("Which button floor was pressed?"); /* Enters which floor it would like to move to */
-                    floorRequested = Integer.parseInt(myObj.nextLine());
+                    System.out.println("Passenger has left elevator at floor " + this.destinationFloor
+                            + " and Elevator now ready for new scheduler request");
+                    this.elevatorHasPassenger = false;              /* Now the elevator has no longer a passenger */
+                }
+                else                                                        /* If not  it must be embarking it */
+                {
+                    System.out.println("Passenger has entered at floor " + this.floorRequested
+                        + " and will be moving to floor: " + this.destinationFloor);
+                    this.elevatorHasPassenger = true;                   /* Now the elevator has a passenger */
                 }
                 this.state = State.DOOR_CLOSING;
                 break;
@@ -84,24 +134,20 @@ public class Elevator {
             /* This state checks if an elevator request is in progress or not.*/
             case State.DOOR_CLOSED:
                 System.out.println("Door closed");
-                if (elevatorButtonPressed) /* If there is an elevator request than it proceeds to move. */
+                if (this.currentFloor != this.destinationFloor) /* If there is an elevator request than it proceeds to move. */
                 {   /* Notify Scheduler that the elevator is being requested by a passenger. Add current floor and destination floor */
-                    if(floorRequested == currentFloor) /* If the elevator is already there do not move*/
-                    {
-                        this.state = State.DOOR_OPENING;
-                    }
-                    else /* else move */
-                    {
-                        this.state = State.ACCELERATING;
-                    }
+                    this.state = State.ACCELERATING;
+
                 }
                 else /* Else gracefully exits and remains IDLE waiting for next Scheduler call. */
                 {
                     /* Notify Scheduler that the elevator is available for next Floor request. For now resting IDLE. */
-                    System.exit(0);
+                    this.state = State.STOP;       /* Make sure that when re-enters state machine it must be at STOP */
+                    return true;                    /* Exited the state machine */
 
                 }
                 break;
+
 
             /* This state always transitions to the Cruising Constant Speed*/
             case State.ACCELERATING:
@@ -119,7 +165,7 @@ public class Elevator {
             case State.DECELERATING:
                 System.out.println("State: Decelerating");
                 state = State.STOP;
-                this.currentFloor = this.floorRequested;
+                this.currentFloor = this.stopFloor;
                 break;
 
             /* Default state in case something goes wrong always transition back to the initial state of STOP */
@@ -127,41 +173,42 @@ public class Elevator {
                 System.out.println("Unknown state");
                 state = State.STOP;
                 break;
+
         }
+        return false;   /* Has not finished the state machine yet. Need to repeat */
+    }
+    /**
+     * Receives messages from the scheduler. It waits for a message,
+     * processes it, and updates the buffer with the result.
+     */
+    private void receiveFromScheduler() {
+        this.buffer = sharedBox.sendToDestination();
+        // Additional processing can be done here
     }
 
     /**
-     *  FUnction responsible for taking the user input.
-     *
-     * @return Boolean representing the user wants to simulate an elevator request or not.
+     * Sends a response back to the scheduler. This method retrieves the processed data
+     * from the buffer and sends it back to the scheduler.
      */
-    public boolean processResponse()
-    {   Scanner myObj = new Scanner(System.in);
-        String userInput;
-        System.out.println("Would you like to simulate a pressed button inside the elevator? (Options:'Yes'/'No')");
-        userInput = myObj.nextLine();
-        return userInput.equals("Yes");
+    private void sendToSchedulerResponse() {
+        sharedBox.getFromDestination(this.buffer);
+    }
+
+    // *The following methods were added for testing purposes*
+
+    /**
+     * getter for buffer
+     * @return Box, the box object
+     */
+    public Box getBuffer() {
+        return buffer;
     }
 
     /**
-     * Main function responsible for creating the object and running the state transition method.
-     *
-     * @param args Optional arguments
+     * setter for the box object
+     * @param buffer, Box object
      */
-    public static void main(String[] args) {
-        Elevator elevator = new Elevator();
-        /* Assume Elevator starts on the button floor of the facility */
-        elevator.currentFloor = 0; /* possibility of doing the same as below for where the elevator starts */
-
-        Scanner myObj = new Scanner(System.in); /* Scanner object to get input from the user to simulate scheduler request */
-        System.out.println("What will be the floor number to simulate the scheduler request?");
-        elevator.floorRequested = Integer.parseInt(myObj.nextLine());
-
-        // Simulate the elevator moving through its states
-        while (true) /* Continuously loop until all the elevator request are done and the one scheduler request. */
-        {
-            elevator.processSchedulerRequest();
-        }
+    public void setBuffer(Box buffer) {
+        this.buffer = buffer;
     }
 }
-
