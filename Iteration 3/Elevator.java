65514aeba0ed127a4e5bf18b61a01d160f1d1f96
IlyesOutaleb <ilyesoutaleb@cmail.carleton.ca>
2024-03-11 21:40:12 -0400

    Feat: Scalable number of elevators with bad algorithm

--- Iteration 3/Elevator.java
+++ Iteration 3/Elevator.java
@@ -1,341 +1,106 @@
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.io.*;
-import java.net.*;
-import java.util.ArrayList;
-
 /**
- * Elevator class handles the operations of an elevator as it receives commands from a scheduler.
- * It runs within its own thread, processing incoming requests and sending responses.
- *
- * @author Al-ameen Alliu
- * @version February 03, 2024,
- * Edited: Ilyes Outaleb (101185290)
- * @version March 02, 2024,
- * Integrated it with Iteration 1
+ * The Floor class represents a floor in a building and implements the Runnable interface, allowing it to be used
+ * as a thread. It reads requests from a csv/txt file, processes them, and communicates with a shared Box object
+ * that is shared with scheduler.
+ * @author Rozba Hakam (101190098)
+ * @author Ilyes Outaleb (101185290)
+ * @version 2024-02-03
  */
-public class Elevator implements Runnable {
-
-    private enum State {
-        STOP,
-        DOOR_OPENING,
-        DOOR_OPEN,
-        DOOR_CLOSING,
-        DOOR_CLOSED,
-        ACCELERATING,
-        CRUISING,
-        DECELERATING,
-    }
-
-    private int floorRequested; /* Assuming the floor requested is retrieved from scheduler */
-
-    private int destinationFloor; /* The floor to which the passenger has asked to go */
-    private int currentFloor; /* Variable representing on which the elevator is currently on. */
-
-    private int stopFloor; /* Buffer variable to represent the destination floor of any given path of the state machine */
-    private State state; /* Initial state is stop */
-
-    private boolean elevatorHasPassenger; /* Boolean to represent whether the elevator is moving with or without passenger */
-
-    private ArrayList<Message> request = new ArrayList<>();
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Floor implements Runnable {
 
-    private DatagramSocket sendReceiveSocket;
-
-    private int schedulerPort = 50000;
-    private int listeningPort = 60000;
-    private InetAddress schedulerAddress;
-    private static final int BUFFER_SIZE = 1024;
-
-    private int id;
+    /* The sharedBox is a reference to the Box object shared with the scheduler. */
+    private Box sharedBox;
 
     /**
-     * Constructor for Elevator class.
+     * Constructor for the Floor class, initializes the sharedBox reference.
      *
+     * @param box The Box object shared with the scheduler.
      */
-    public Elevator(String schedulerIP, int schedulerPort, int id) {
-
-        this.state = State.STOP;
-        this.currentFloor = 0;
-        this.elevatorHasPassenger = false;
-        this.id = id;
-
-
-        this.schedulerPort = schedulerPort;
-        try {
-            this.schedulerAddress = InetAddress.getByName(schedulerIP);
-            this.sendReceiveSocket = new DatagramSocket(listeningPort + id);
-            System.out.println("ElevatorSubsystem " + this.id + " listening on port: " + listeningPort + id);
-        } catch (Exception e) {
-            System.err.println("Error initializing DatagramSocket: " + e.getMessage());
-            System.exit(1);
-        }
-    }
-
-    /**
-     * The main run method that is executed when the elevator thread starts.
-     * It continuously processes requests from the scheduler and sends responses.
-     */
-    public void run() {
-        //System.out.println("HI");
-        int count = 0;
-        //while(true) {
-
-        //System.out.println("================ REQUESTS IN ELEVATOR " + this.id + "QUEUE ============================");
-        Message temp;
-        do {
-
-            temp = receiveFromScheduler();
-            if (temp.getConfirmation() == false)
-            {
-                temp.horizontalPrint(this.id);
-                this.request.add(temp);
-                count ++;
-            }
-        } while(temp.getConfirmation() == false);
-        System.out.println("================ END ============================");
-
-        System.out.println("Count" + count);
-        //for(Message currentRequest : this.request)
-
-        for (int i = 0; i < count; i++)
-        {   System.out.println("=========== Executing request: ");
-            this.request.get(i).horizontalPrint(this.id);
-
-            this.floorRequested = this.request.get(i).getSource(); /* Retrieves the data from the box to be used */
-            this.destinationFloor = this.request.get(i).getDestination();
-
-            System.out.println("The Elevator " + this.id + " is starting at floor: " + this.currentFloor);
-
-            /* Continues while the state machine has not finished marked by the arrival of the passenger at destination */
-            while (!processSchedulerRequest()) {
-                /* If the elevator is where the passenger is pressing the floor button */
-                if (this.currentFloor == this.floorRequested)
-                {
-                    /* Then elevator must move to where the passenger wants to debark */
-                    this.stopFloor = this.destinationFloor;
-
-                }
-                else /* if that is not the case */
-                {   /* then it must move to where the passenger is located first */
-                    this.stopFloor = this.floorRequested;
-                }
-            }
-            System.out.println("");// only for one elevator change it to multiple by looking at iteration 2 original.
-
-            this.request.get(i).setConfirmation(true);
-            System.out.println("Finished moving on");
-            System.out.println(" ");
-        }
-
-        System.out.println("Finished list of requests. Now available for new.");
-        sendToSchedulerResponse(this.request.get(count-1));
-        //}
-    }
-
-
-    public boolean processSchedulerRequest() {
-
-        switch (this.state) {
-            /* Will start at Stop and will transisient depending if its on the right floor or not */
-            case State.STOP:
-
-                System.out.println(" State: Stop ");
-                System.out.print(" Current Floor: " + this.currentFloor + " -> ");
-
-                /* If elevator is in requested or destination floor it must open to let passenger in or out */
-                if (currentFloor == floorRequested || currentFloor == destinationFloor)
-                {
-                    this.state = State.DOOR_OPENING; /* If its on right floor just open door */
-                }
-                else
-                {
-                    this.state = State.ACCELERATING; /* If its on wrong floor move to the appropriate one */
-                }
-                break;
-
-            /* This state always transitions to the Doors being opened */
-            case State.DOOR_OPENING:
-                System.out.print(" State: Door opening " + " -> ");
-                this.state = State.DOOR_OPEN;
-                break;
-
-            /* This state is where the elevator has its door opens. Then checks if passenger entered */
-            case State.DOOR_OPEN:
-                System.out.println(" State: Door open ");
-
-                if (this.elevatorHasPassenger)             /* If the elevator has a passenger it must be debarking it */
-                {
-                    System.out.print("Passenger has left elevator at floor " + this.destinationFloor
-                            + " and Elevator now ready for new scheduler request " +  " -> ");
-                    this.elevatorHasPassenger = false;              /* Now the elevator has no longer a passenger */
-                }
-                else                                                        /* If not  it must be embarking it */
-                {
-                    System.out.print("Passenger has entered at floor " + this.floorRequested
-                        + " and will be moving to floor: " + this.destinationFloor + " -> ");
-                    this.elevatorHasPassenger = true;                   /* Now the elevator has a passenger */
-                }
-                this.state = State.DOOR_CLOSING;
-                break;
-
-            /* This state always transitions to the Doors being Closed */
-            case State.DOOR_CLOSING:
-                System.out.print(" State: Door closing " + " -> ");
-                state = State.DOOR_CLOSED;
-                break;
-
-            /* This state checks if an elevator request is in progress or not.*/
-            case State.DOOR_CLOSED:
-                System.out.print(" Door closed " + " -> ");
-                if (this.currentFloor != this.destinationFloor) /* If there is an elevator request than it proceeds to move. */
-                {   /* Notify Scheduler that the elevator is being requested by a passenger. Add current floor and destination floor */
-                    this.state = State.ACCELERATING;
-
-                }
-                else /* Else gracefully exits and remains IDLE waiting for next Scheduler call. */
-                {
-                    /* Notify Scheduler that the elevator is available for next Floor request. For now resting IDLE. */
-                    this.state = State.STOP;       /* Make sure that when re-enters state machine it must be at STOP */
-                    return true;                    /* Exited the state machine */
-
-                }
-                break;
-
-
-            /* This state always transitions to the Cruising Constant Speed*/
-            case State.ACCELERATING:
-                System.out.print(" State: Accelerating " + " -> ");
-                state = State.CRUISING;
-                break;
-
-            /* This state always transitions to the Decelerating state*/
-            case State.CRUISING:
-                System.out.print(" State: Cruising "  + " -> ");
-                state = State.DECELERATING;
-                break;
-
-            /* This state always transitions to the Decelerating state*/
-            case State.DECELERATING:
-                System.out.print("State: Decelerating"  + "->");
-                state = State.STOP;
-                this.currentFloor = this.stopFloor;
-                break;
-
-            /* Default state in case something goes wrong always transition back to the initial state of STOP */
-            default:
-                System.out.println("Unknown state");
-                state = State.STOP;
-                break;
-
-        }
-        return false;   /* Has not finished the state machine yet. Need to repeat */
-    }
-    /**
-     * Receives messages from the scheduler. It waits for a message,
-     * processes it, and updates the buffer with the result.
-     */
-    private Message receiveFromScheduler() {
-        return receiveUDPMessage(); // Blocking call, waits for a message
-
-    }
-
-    private Message receiveUDPMessage() {
-        try {
-            byte[] buffer = new byte[BUFFER_SIZE];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            //System.out.println("ElevatorSubsystem ready to receive UDP message on port: " + listeningPort);
-
-            sendReceiveSocket.receive(packet);
-
-            ByteArrayInputStream bis = new ByteArrayInputStream(buffer);
-            ObjectInputStream in = new ObjectInputStream(bis);
-            Message message = (Message) in.readObject();
-
-            //System.out.println("Received object:");
-            //message.printMessage();
-
-
-            return message;
-        } catch (Exception e) {
-            System.err.println("ElevatorSubsystem failed to receive UDP message: " + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * Sends a response back to the scheduler. This method retrieves the processed data
-     * from the buffer and sends it back to the scheduler.
-     */
-    private void sendToSchedulerResponse(Message messageToBeSent) {
-        sendUDPMessage(messageToBeSent);
-    }
-
-    public void sendUDPMessage(Message message) {
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutputStream out = new ObjectOutputStream(bos);
-            out.writeObject(message);
-            out.flush();
-            byte[] serializedData = bos.toByteArray();
-
-            DatagramPacket packet = new DatagramPacket(serializedData, serializedData.length, schedulerAddress, schedulerPort);
-            sendReceiveSocket.send(packet);
-        } catch (IOException e) {
-            log("Error sending command to elevator: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Logs a message to the standard output, prefixed with the class name.
-     * @param message The message to log.
-     */
-    private static void log(String message) {
-        System.out.println("[ElevatorSubsystem] " + message);
-    }
-
-    public void closeSocket() {
-        if (sendReceiveSocket != null && !sendReceiveSocket.isClosed()) {
-            sendReceiveSocket.close();
-            log("Socket closed.");
-        }
-    }
-
-    public static void main(String[] args) {
-        String schedulerIP = "localhost";
-        int schedulerPort = 50000;
-        if (args[0].equals("Elevator1"))
-        {
-            Elevator elevatorSubsystem = new Elevator(schedulerIP, schedulerPort, 1);
-            new Thread(elevatorSubsystem).start();
-            // Register shutdown hook
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                elevatorSubsystem.closeSocket();
-                log("SchedulerSubsystem shutdown hook executed.");
-            }));
-        } else if (args[0].equals("Elevator2"))
-        {
-            Elevator elevatorSubsystem2 = new Elevator(schedulerIP, schedulerPort, 2);
-            new Thread(elevatorSubsystem2).start();
-            // Register shutdown hook
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                elevatorSubsystem2.closeSocket();
-                log("SchedulerSubsystem shutdown hook executed.");
-            }));
-        }
-
-    }
-
-    public int getCurrentFloor()
+    public Floor(Box box)
     {
-        return this.currentFloor;
+        this.sharedBox = box;
     }
-    // *The following methods were added for testing purposes*
 
+    /**
+     * The run method is called when a floor is executed as a thread. It reads requests
+     * from a file, processes them, and terminates the program afterward.
+     */
+    public void run()
+    {
+        readFileAndSendRequests("Requests.txt");
+        System.exit(0);
+    }
 
+    /**
+     * Reads requests from a csv/txt file, processes each line, and interacts with the shared Box.
+     *
+     * @param filename The name of the file containing the requests.
+     */
+    private void readFileAndSendRequests(String filename)
+    {
+        try {
+            File requestFile = new File(filename);
+            Scanner scanner = new Scanner(requestFile);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                createBoxFromCSV(line);
+                getFromSchedulerResponse();
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+    }
+
+    /**
+     * divide each CSV/txt line, creates a Box object from the data, and sends it to the shared Box.
+     *
+     * @param line The CSV line stores the requested information.
+     */
+    private void createBoxFromCSV(String line)
+    {
+        /* Parse the CSV line and create a Box object */
+        String[] data = line.split(",");
+        String time = data[0].trim();
+        int floorNumber = Integer.parseInt(data[1].trim());
+        String direction = data[2].trim();
+        int carButtonNumber = Integer.parseInt(data[3].trim());
+        boolean directionUp;
+
+        /* ternary Operator to assign the direction up a boolean value */
+        directionUp = (direction.equals("Up")) ? true : false;
+
+        getFromCSV(time, floorNumber, directionUp, carButtonNumber);
+    }
+
+    /**
+     * Creates a Box object with the provided message information and sends it to the shared Box.
+     *
+     * @param time The time of the request.
+     * @param floorNumber The floor number of the request.
+     * @param directionUp The direction of the request (Up = true) or (Down = false).
+     * @param carButtonNumber The car button number associated with the request.
+     */
+    private void getFromCSV(String time, int floorNumber, boolean directionUp, int carButtonNumber)
+    {
+        Box temp = new Box(time, floorNumber, directionUp, carButtonNumber);
+        sharedBox.getFromSource(temp);
+    }
+
+    /**
+     * gets a Box object from the shared Box and prints its contents.
+     */
+    private void getFromSchedulerResponse()
+    {
+        Box temp = sharedBox.sendToSource();
+        temp.printContents();
+    }
 }
+
