6ba9f3d394277c91e8eed7a0f0d25dd84c9d2ff8
Taran Basati <TaranBasati@cmail.carleton.ca>
2024-04-08 01:40:03 -0400

    Update ElevatorManager.java

--- Iteration5/ElevatorManager.java
+++ Iteration5/ElevatorManager.java
@@ -1,6 +1,5 @@
 public class ElevatorManager {
 
-
     private static void log(String message) {
         System.out.println("[FloorSubsystem] " + message);
     }
@@ -9,15 +8,14 @@ public class ElevatorManager {
     public static void main(String[] args) {
         String schedulerIP = "localhost";
         int schedulerPort = 50000;
-
         Elevator elevatorSubsystem = new Elevator(schedulerIP, schedulerPort, 1);
         new Thread(elevatorSubsystem).start();
         // Register shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             elevatorSubsystem.closeSocket();
             log("SchedulerSubsystem shutdown hook executed.");
+            log(elevatorSubsystem.measurements());
         }));
-
 
         Elevator elevatorSubsystem2 = new Elevator(schedulerIP, schedulerPort, 2);
         new Thread(elevatorSubsystem2).start();
@@ -25,8 +23,8 @@ public class ElevatorManager {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             elevatorSubsystem2.closeSocket();
             log("SchedulerSubsystem shutdown hook executed.");
+            log(elevatorSubsystem2.measurements());
         }));
-
 
         Elevator elevatorSubsystem3 = new Elevator(schedulerIP, schedulerPort, 3);
         new Thread(elevatorSubsystem3).start();
@@ -34,6 +32,7 @@ public class ElevatorManager {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             elevatorSubsystem3.closeSocket();
             log("SchedulerSubsystem shutdown hook executed.");
+            log(elevatorSubsystem3.measurements());
         }));
 
         Elevator elevatorSubsystem4 = new Elevator(schedulerIP, schedulerPort, 4);
@@ -42,8 +41,7 @@ public class ElevatorManager {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             elevatorSubsystem4.closeSocket();
             log("SchedulerSubsystem shutdown hook executed.");
+            log(elevatorSubsystem4.measurements());
         }));
-
     }
 }
-
